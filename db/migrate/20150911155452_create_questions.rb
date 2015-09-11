b6a88b20b9c4ca54d9b221f8f836a0433ea7bcfc
leahgoldberg <leahgoldberg31@gmail.com>
2015-09-11 16:28:33 -0400

    add references to tables

--- db/migrate/20150911155452_create_questions.rb
+++ db/migrate/20150911155452_create_questions.rb
@@ -3,6 +3,7 @@ class CreateQuestions < ActiveRecord::Migration
     create_table :questions do |t|
     	t.string :title, null:false, limit:150
     	t.text :content, null:false, limit:5000
+    	t.references :submitter
     	t.integer :best_answer_id
       t.timestamps
     end
--- db/migrate/20150911160547_create_answers.rb
+++ db/migrate/20150911160547_create_answers.rb
@@ -2,6 +2,8 @@ class CreateAnswers < ActiveRecord::Migration
   def change
     create_table :answers do |t|
     	t.text :content, null:false, limit:2000	
+    	t.references :responder
+    	t.references :question
       t.timestamps
     end
   end
