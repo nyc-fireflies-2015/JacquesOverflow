6a7616f172e5fc50faeb34798780ca4bb9710052
leahgoldberg <leahgoldberg31@gmail.com>
2015-09-11 16:45:41 -0400

    add tests for answer model

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  	context 'contains valid data' do 	
+	  it { expect(subject).to validate_presence_of(:content) }
+	  it { expect(subject).to validate_length_of(:content) }
+	end
+
+	context 'has correct associations' do 
+		it { expect(subject).to have_many(:votes) }
+		it { expect(subject).to have_many(:comments) }
+		it { expect(subject).to belong_to(:question) }
+		it { expect(subject).to belong_to(:responder) }
+	end	
 end
