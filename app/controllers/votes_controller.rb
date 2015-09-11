f46f8233d41f6cc18f5120bd4ad3fe95de48945e
aderend <d.dogrusoy@gmail.com>
2015-09-11 18:13:18 -0400

    still WIP

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,9 +1,8 @@
 class VotesController < ApplicationController
   def create
     if params[:questions]
-      @voteable = Question.find_by(id: @voteable.id)
+      @voteable = Question.find_by(vote_params)
       vote = Vote.new(vote_params)
-      #implmement a user helper method here; current_user etc..
       vote.voter_id = current_user
       if vote.save
         redirect_to question_path
@@ -14,7 +13,6 @@ class VotesController < ApplicationController
 
     end
 
-
   end
 
   def destroy
@@ -23,7 +21,7 @@ class VotesController < ApplicationController
   private
 
     def vote_params
-      params.require(:vote).permit(:voteable_id, :voter_id, :voteable_type)
+      params.require(:vote).permit(:voteable_id, :voter_id, :voteable_type, :value)
     end
 
 end
