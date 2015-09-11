92e36f5564eae84a480db065b2965b554ea77600
Abby <email": null>
2015-09-11 16:52:36 -0400

    add #new and #create in questions controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,6 +4,26 @@ class QuestionsController < ApplicationController
   end
 
   def show
-    @question = Question.find(params[:id])
+    @question = Question.find_by(params[:id])
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    question = Question.new(question_params)#merge current user
+    if question.save
+      redirect_to root_path
+    else
+      flash[:error] = "Invalid input: must include both title and content."
+      redirect_to new_question_path
+    end
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:title, :content)
   end
 end
