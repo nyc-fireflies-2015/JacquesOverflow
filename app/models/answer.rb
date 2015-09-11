b1f03341f21beb4b79a2b6d13c83770d7ef1c76e
leahgoldberg <leahgoldberg31@gmail.com>
2015-09-11 15:41:17 -0400

    add length validations to models

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,5 +1,6 @@
 class Answer < ActiveRecord::Base
 	validates_presence_of :content
+	validates :content, length: {maximum: 2000}
 
 	has_many :comments, as: :commentable
 	has_many :votes, as: :voteable
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,8 @@
 class Question < ActiveRecord::Base
 
 	validates_presence_of :content, :title
+	validates :title, length: {maximum: 150}
+	validates :content, length: {maximum: 5000}
 
 	belongs_to :submitter, class_name: :User
 	has_many :answers
