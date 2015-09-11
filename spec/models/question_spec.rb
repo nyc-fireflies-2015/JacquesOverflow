3d35a402dcfa6d6dc630859ef0c6a70229eb0335
leahgoldberg <leahgoldberg31@gmail.com>
2015-09-11 16:22:12 -0400

    add tests for question validations

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+	context 'contains valid data' do 	
+	  it { expect(subject).to validate_presence_of(:content) }
+	  it { expect(subject).to validate_presence_of(:title) }
+	  it { expect(subject).to validate_length_of(:content) }
+	  it { expect(subject).to validate_length_of(:title) }
+	end
+
+	context 'has correct associations' do 
+	end	
 end
+
