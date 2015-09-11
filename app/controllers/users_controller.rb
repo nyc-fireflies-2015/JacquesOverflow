66dab90b6f81c0c9620546064a5d4f4a6a8f5d0a
Travis Allen <email": null>
2015-09-11 19:15:27 -0400

    registering starts new session (logs user in)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,10 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
-    if @user.save
-      redirect_to '/'
+    user = User.new(user_params)
+    if user.save
+      log_in(user)
+      redirect_to root_url
     else
       render 'new'
     end
