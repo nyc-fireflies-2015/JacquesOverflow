74a932662c1899329ddd441a821197896b230c3a
Albalenys <albalenysd@gmail.com>
2015-09-12 18:08:54 -0400

    add rspec tests for questions#update

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe QuestionsController, type: :controller do
+  include SessionsHelper
   let(:question) { FactoryGirl.create :question }
   let(:user) { FactoryGirl.create :user }
 
@@ -20,6 +21,54 @@ RSpec.describe QuestionsController, type: :controller do
     it "assigns the requested question to @question" do
       get :show, id: question
       expect(assigns(:question)).to eq(question)
+    end
+  end
+
+  describe "PUT #update" do
+    before :each do
+      @question = FactoryGirl.create(:question, title: "Title", content: "content")
+    end
+
+    context "valid attributes" do
+      it "located the requested @question" do
+        put :update, id: @question
+        expect(assigns(:question)).to eq(@question)
+      end
+
+      it "changes @question's attributes" do
+        log_in(user)
+        put :update, id: @question, question: FactoryGirl.attributes_for(:question, title: "Updated Title")
+        @question.reload
+        expect(@question.title).to eq("Updated Title")
+      end
+
+      it "redirects to the updated question" do
+        log_in(user)
+        put :update, id: @question, question: FactoryGirl.attributes_for(:question)
+        expect(response).to redirect_to @question
+      end
+    end
+
+    context "invalid attributes" do
+
+      it "locates the requested @question" do
+        put :update, id: @question
+        expect(assigns(:question)).to eq(@question)
+      end
+
+      it "does not change @question's attributes" do
+        log_in(user)
+        put :update, id: @question, question: FactoryGirl.attributes_for(:question, title: nil)
+        @question.reload
+        expect(@question.title).to eq("Title")
+        expect(@question.content).to eq("content")
+      end
+
+      it "redirects to edit view" do
+        log_in(user)
+        put :update, id: @question, question: FactoryGirl.attributes_for(:question, title: nil)
+        expect(response).to redirect_to edit_question_path(@question)
+      end
     end
   end
 end
