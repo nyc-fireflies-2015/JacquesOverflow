2c820d3cac073586a7c986502aeaf3bbff2cc97c
leahgoldberg <leahgoldberg31@gmail.com>
2015-09-12 18:11:25 -0400

    add french titles

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,35 @@
-user = User.create(username: 'ddog', email: 'ddogrusoy@gmail.com', password: 'password')
+jacques = User.create(
+	username: 'Jacques', 
+	email: 'jacques@gmail.com', 
+	password: 'password',
+	bio: "je suis Jacques",
+	avatar_url: Faker::Avatar.image)
 
-user2 = User.create(username: 'Jacques', email: 'jacques@gmail.com', password: 'password')
+100.times { User.create(
+	username: Faker::Internet.user_name,
+	email: Faker::Internet.email,
+	bio: "Cet utilisateur aime à être mystérieux",
+	password: "supersecure",
+	avatar_url: Faker::Avatar.image
+	)
+}
 
-questions = [['what is activerecord ?', "someone explain it to me please!!!"], ['how does this git thingie work?', 'I accidently lost all my work, teammates are pissed!!?']]
+
+
+titles = ['Quel est activerecord? Je ne comprends pas!', 
+	"Comment cela fonctionne-chose git??", 
+	"Comment faire --no-ri --no-rdoc défaut pour gem install?",
+	"Une explication concise de néant v. nil v. empty dans Ruby on Rails",
+	"Quel est attr_accessor en Ruby?",
+	"Comment puis-je retirer RVM (Ruby Version Manager) de mon système?",
+	"Pourquoi les gens utilisent Heroku lorsque AWS est présent? Qu'est-ce à propos de la distinction Heroku",
+	"Purger ou recréer une base de données Ruby on Rails",
+	"Ruby: qu'est-ce que w% (array) signifie?",
+	"Pas de matches de route \"/users/sign_out\" concevoir des rails 3"
+	]
+
+
+"someone explain it to me please!!!"], ['how does this git thingie work?', 'I accidently lost all my work, teammates are pissed!!?']]
 
 answers = ["it's for databases and shit", "Go read the documentation noob"]
 
