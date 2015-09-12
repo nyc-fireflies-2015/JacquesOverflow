5aa910a4073ce212b394819a06ca0cc891e1e006
Albalenys <albalenysd@gmail.com>
2015-09-12 16:07:46 -0400

    add rspec tests for questions#index and questions#show

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe QuestionsController, type: :controller do
+  let(:question) { FactoryGirl.create :question }
+  let(:user) { FactoryGirl.create :user }
 
+  describe "GET #index" do
+    it "renders the :index view" do
+      get :index
+      expect(response).to render_template :index
+    end
+  end
+
+  describe "GET #show" do
+    it "renders the :show view" do
+      get :show, id: question
+      expect(response).to render_template :show
+    end
+
+    it "assigns the requested question to @question" do
+      get :show, id: question
+      expect(assigns(:question)).to eq(question)
+    end
+  end
 end
