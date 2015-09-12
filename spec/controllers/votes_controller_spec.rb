dd240b13d75d6fb155188703da0b7733829e7a36
Travis Allen <email": null>
2015-09-12 17:56:05 -0400

    get first test to pass

--- spec/controllers/votes_controller_spec.rb
+++ spec/controllers/votes_controller_spec.rb
@@ -1,13 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe VotesController, type: :controller do
+  def create_question_upvote
+    @question = FactoryGirl.create(:question)
+    @user = FactoryGirl.create(:user)
+    @vote = Vote.new(value: 1)
+    @vote_attributes = @vote.attributes
+
+  end
+
   describe "#new" do
   end
 
 
   describe 'POST #create' do
     context 'with question params' do
-
+      it 'adds a vote to a question' do
+        create_question_upvote
+        post :create, question_id: @question.id, vote: @vote_attributes
+        expect(@question.votes.count).to eq(1)
+      end
     end
 
     context 'with answer params' do
