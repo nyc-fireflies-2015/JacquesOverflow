bcd73ce64c1105ae696ded96b580ae698f191f91
leahgoldberg <leahgoldberg31@gmail.com>
2015-09-12 14:38:27 -0400

    update comment controller so you can add comment to either question or answer

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,30 +1,28 @@
 class CommentsController < ApplicationController
-  before_action :find_question
   before_action :authenticate_user
 
   def create
-    comment = Comment.new(comment_params)
-    if comment.save
-      redirect_to question_path(question)
-    else
-      flash[:error] = "Comment must be 1500 chars or less."
-      redirect_to question_path(question)
+    if params[:answer_id]
+      answer = Answer.find_by(id: params[:answer_id])
+      @question = answer.question
+      comment = answer.comments.build(comment_params.merge(commentator: current_user))
+    elsif params[:question_id]
+      @question = Question.find_by(id: params[:question_id])
+      comment = question.comments.build(comment_params.merge(commentator: current_user))
     end
+    flash[:error] = "Comment must be 1500 chars or less." unless comment.save
+    redirect_to question_path(@question)
   end
 
   def destroy
     Comment.find(params[:id]).destroy
-    redirect_to question_path(question)
+    redirect_to question_path(@question)
   end
 
   private
 
   def comment_params
-    params.require(:comment).permit(:text)
-  end
-
-  def find_question
-    question = Question.find(comment.id)
+    params.require(:comment).permit(:content)
   end
 
   def authenticate_user
