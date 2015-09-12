3e0851dfe05a9e1ea79788676224fe50088385d6
Travis Allen <email": null>
2015-09-12 16:51:14 -0400

    get started on vote controller spec

--- spec/controllers/votes_controller_spec.rb
+++ spec/controllers/votes_controller_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe VotesController, type: :controller do
+  describe "#new" do
+  end
+
+
+  describe 'POST #create' do
+    context 'with question params' do
+
+    end
+
+    context 'with answer params' do
+    end
+
+  end
+
 
 end
+
+
+# describe '#create' do
+#     it 'changes the total number of articles' do
+#       expect do
+#         post :create, {article: FactoryGirl.attributes_for(:article)}
+#       end.to change{Article.all.length}
+#     end
+#   end
+
+  # describe "#create" do
+  #   it 'redirects to root_path after voting on a question' do
+  #     expect do
+  #       question = FactoryGirl.create(:question)
+  #       user = FactoryGirl.create(:user)
+  #       vote = question.votes.build(value: 1, voter: user)
+  #       post :create, {vote: vote.create(value:1)}
+  #     end.to change{Vote.all.length}
+  #   end
