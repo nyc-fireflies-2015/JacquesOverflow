aaf02015c95b9494f98c118b13deaa53d5cd93a2
Travis Allen <email": null>
2015-09-13 11:32:48 -0400

    replace non US-ASCII characters to debug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.new(user_params.merge(avatar_url: "http://wiseheartdesign.com/images/articles/default-avatar.png", bio: "Cet utilisateur aime à etre mystérieux"))
+    user = User.new(user_params.merge(avatar_url: "http://wiseheartdesign.com/images/articles/default-avatar.png", bio: "Cet utilisateur aime a etre mysterieux"))
     if user.save
       log_in(user)
       redirect_to root_path, notice: "Account Has Created!!"
