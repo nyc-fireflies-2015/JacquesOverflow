dbd1c85c93ad33350ad54f66eb5a4f517df0ff56
leahgoldberg <leahgoldberg31@gmail.com>
2015-09-13 16:29:40 -0400

    add tests for deleting comment from question and answer

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -19,7 +19,22 @@ RSpec.describe CommentsController, type: :controller do
 		@comment_attributes = FactoryGirl.attributes_for(:comment)
 	 end	
 
-	 describe "POST #create" do
+	 def destroy_question_comment
+	 	log_in(user)
+		@question = FactoryGirl.create(:question)
+		@comment = FactoryGirl.create(:comment)
+		@question.comments << @comment
+	 end
+	 
+	 def destroy_answer_comment
+	 	log_in(user)
+		@question = FactoryGirl.create(:question)
+		@answer = @question.answers.create(FactoryGirl.attributes_for(:answer))
+		@comment = FactoryGirl.create(:comment)
+		@answer.comments << @comment
+	 end	
+
+	 describe "#create" do
       it "redirects to root if not logged in" do
       	question = FactoryGirl.create(:question)
         post :create, comment: FactoryGirl.attributes_for(:comment), question_id: question
@@ -53,7 +68,40 @@ RSpec.describe CommentsController, type: :controller do
         expect(response).to redirect_to(question_path(@question))
       end
     end
-
   end  
+
+  describe "#destroy" do 
+    it "redirects to root page if not logged in" do
+      delete :destroy, id: comment, question_id: FactoryGirl.create(:question)
+      expect(response).to redirect_to root_path
+    end
+
+    context "deletes comments from a question" do 
+    	it "deletes the question comment" do 
+    		destroy_question_comment
+    		expect{delete :destroy, id: @comment, question_id: @question}.to change{@question.comments.count}.by(-1)
+    	end
+    	
+    	it "redirects to the question show page after deletion" do
+    		destroy_question_comment
+    		delete :destroy, id: @comment, question_id: @question
+    		expect(response).to redirect_to(question_path(@question))
+    	end	
+    end
+    
+    context "deletes comments from an answer" do 
+    	it "deletes the answer comment" do 
+    		destroy_answer_comment
+    		expect{delete :destroy, id: @comment, answer_id: @answer}.to change{@answer.comments.count}.by(-1)
+    	end
+    	
+    	it "redirects to the answer show page after deletion" do
+    		destroy_answer_comment
+    		delete :destroy, id: @comment, answer_id: @answer
+    		expect(response).to redirect_to(question_path(@question))
+    	end	
+    end	
+
+  end	
 
 end
