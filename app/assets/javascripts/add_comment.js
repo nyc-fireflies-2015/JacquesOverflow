3406a772da27bb1edbdaabfbd8893412d345e7a4
aderend <d.dogrusoy@gmail.com>
2015-09-13 17:08:40 -0400

    ajax adding question comments

--- app/assets/javascripts/add_comment.js
+++ app/assets/javascripts/add_comment.js
@@ -2,14 +2,18 @@ $(document).ready(function(){
   $('#new_comment').on('submit',function(e){
     e.preventDefault();
     $this = $(this);
+    // debugger
     $.ajax({
       url: $this.attr('action'),
       data: $this.serialize(),
-      method: 'POST'
+      method: $this.attr('method')
     }).done(function(response){
-      ('.question-comments-list').append(response)
-    }).fail(function(){
-      console.log("I failed")
+      $(".question-comments-list").append(response);
+    }).fail(function(response){
+      console.log(response)
     })
   })
+
+  $('#new_answer').on('submit')
+
 })
