18dd2fe0e20c6132244a30b41e914fe6ba221a26
leahgoldberg <leahgoldberg31@gmail.com>
2015-09-13 15:55:13 -0400

    add tests for creating question comment

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe CommentsController, type: :controller do
+	 include SessionsHelper
+
+	 let(:user) { FactoryGirl.create :user }
+	 let(:comment) {FactoryGirl.create :comment}
+
+	 def create_question_comment
+		log_in(user)
+		@question = FactoryGirl.create(:question)
+		@comment_attributes = FactoryGirl.attributes_for(:comment)
+	 end	
+
+	 describe "POST #create" do
+      it "redirects to root if not logged in" do
+      	question = FactoryGirl.create(:question)
+        post :create, comment: FactoryGirl.attributes_for(:comment), question_id: question
+        expect(response).to redirect_to root_path
+      end
+
+    context "valid attributes for question comment" do
+      it "creates a new question comment" do
+        create_question_comment
+        expect{post :create, comment: @comment_attributes, question_id: @question}.to change{@question.comments.count}.by(1)
+        expect(Comment.last).to have_attributes(@comment_attributes)
+      end
+
+      it "redirects back to the question show page" do
+        create_question_comment
+        post :create, comment: @comment_attributes, question_id: @question
+        expect(response).to redirect_to(question_path(@question))
+      end
+    end
+  end  
 
 end
--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :comment do
-    
+     content {Faker::Lorem.paragraph}
   end
 
 end
