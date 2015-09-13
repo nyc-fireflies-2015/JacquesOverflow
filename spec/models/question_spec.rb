2a67bf013f3a257e8a57403fc87abd276cca7986
leahgoldberg <leahgoldberg31@gmail.com>
2015-09-13 17:43:25 -0400

    add tests for checking answer rating

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -15,5 +15,25 @@ RSpec.describe Question, type: :model do
 		it { expect(subject).to have_many(:votes) }
 		it { expect(subject).to belong_to(:submitter) }
 	end	
+
+	let(:question) { FactoryGirl.create(:question) }
+
+	describe 'rating' do 
+		it 'should have a rating of 0 if it has 1 upvote and 1 downvote' do
+			question.votes.create(value: 1)
+			question.votes.create(value: -1)
+			expect(question.rating).to eq(0)
+		end	
+
+		it 'should have a rating of -2 with two downvotes' do
+			2.times {question.votes.create(value: -1)}
+			expect(question.rating).to eq(-2)
+		end
+		
+		it 'should have a rating of 4 with four upvotes' do
+			4.times {question.votes.create(value: 1)}
+			expect(question.rating).to eq(4)
+		end	
+	end	
 end
 
