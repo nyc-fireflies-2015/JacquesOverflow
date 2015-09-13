b82bad1fa200575520a152ee0eeb2ca43157e6f2
leahgoldberg <leahgoldberg31@gmail.com>
2015-09-13 17:49:28 -0400

    add tests to check if question has a best answer or not

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -18,7 +18,7 @@ RSpec.describe Question, type: :model do
 
 	let(:question) { FactoryGirl.create(:question) }
 
-	describe 'rating' do 
+	describe '#rating' do 
 		it 'should have a rating of 0 if it has 1 upvote and 1 downvote' do
 			question.votes.create(value: 1)
 			question.votes.create(value: -1)
@@ -33,6 +33,18 @@ RSpec.describe Question, type: :model do
 		it 'should have a rating of 4 with four upvotes' do
 			4.times {question.votes.create(value: 1)}
 			expect(question.rating).to eq(4)
+		end	
+	end	
+
+	describe "#has_best_answer?" do 
+		it 'should return false upon creation' do 
+			expect(question.has_best_answer?).to be_falsey
+		end
+		
+		it 'should return true after user chooses best answer' do
+			answer = question.answers.create(FactoryGirl.attributes_for(:answer).merge(best_answer: true))
+			question.best_answer_id = answer.id
+			expect(question.has_best_answer?).to be_truthy
 		end	
 	end	
 end
