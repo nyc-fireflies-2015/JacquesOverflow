93191068bb90f95f357fd5ab854f55b9643a13f0
leahgoldberg <leahgoldberg31@gmail.com>
2015-09-13 16:12:12 -0400

    add tests for creating answer comment

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -4,11 +4,18 @@ RSpec.describe CommentsController, type: :controller do
 	 include SessionsHelper
 
 	 let(:user) { FactoryGirl.create :user }
-	 let(:comment) {FactoryGirl.create :comment}
+	 let(:comment) { FactoryGirl.create :comment }
 
 	 def create_question_comment
 		log_in(user)
 		@question = FactoryGirl.create(:question)
+		@comment_attributes = FactoryGirl.attributes_for(:comment)
+	 end	
+
+	 def create_answer_comment
+	 	log_in(user)
+	 	@question = FactoryGirl.create(:question)
+		@answer = @question.answers.create(FactoryGirl.attributes_for(:answer))
 		@comment_attributes = FactoryGirl.attributes_for(:comment)
 	 end	
 
@@ -32,6 +39,21 @@ RSpec.describe CommentsController, type: :controller do
         expect(response).to redirect_to(question_path(@question))
       end
     end
+
+    context "valid attributes for answer comment" do
+      it "creates a new answer comment" do
+        create_answer_comment
+        expect{post :create, comment: @comment_attributes, answer_id: @answer.id}.to change{@answer.comments.count}.by(1)
+        expect(Comment.last).to have_attributes(@comment_attributes)
+      end
+
+      it "redirects back to the question show page" do
+        create_answer_comment
+        post :create, comment: @comment_attributes, answer_id: @answer.id
+        expect(response).to redirect_to(question_path(@question))
+      end
+    end
+
   end  
 
 end
